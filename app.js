9d4a1178b887dbc33a596e507568e083b0645207
pauline-devavry <pauline.devavry@oclock.school>
2024-06-10 15:35:23 +0200

    mise en place

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// vu que j'utilise le moteur de template ejs, je le précise à express
+app.set("view engine", "ejs");
+// ! on reviendra la dessus pour parler de sécurité
+app.set('views', 'app/views');
+
+// middleware pour préciser que les fichiers statiques des requêtes devront être trouvé dans le dossier public
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on PORT ${process.env.PORT}`);
+    });
+    
